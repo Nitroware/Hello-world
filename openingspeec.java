433d9b961a7852eac06a55b7348597c464e69cb0
MasterofBasics <kingender91@gmail.com>
2016-10-16 19:57:11 -0700

    Create openingspeec.java

---
+++ openingspeec.java
@@ -0,0 +1,11 @@
+public class openingspeech {
+        public static void main(String[] args) throws InterruptedException {
+                System.out.println("October 16, 2016 was the day Nitroware was founded, and it was the start of a new organization of GitHub.");
+                Thread.sleep(5000);
+                System.out.println("The organization will work on coding and creating cool things.");
+                Thread.sleep(5000);
+                System.out.println("Megalegacy is welcome to invite anyone who wishes to join.");
+                Thread.sleep(5000);
+                System.out.println("Come back soon, we might have some cool stuff planned...");
+        }
+}
